764e369091b9fa7b0e74c2dd67f7dfee09c45c47
dana nur fiqi <danufiq98@gmail.com>
2022-11-16 06:45:16 +0700

    init proj

---
+++ SephoraMobile/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SephoraMobile
+//
+//  Created by dana nur fiqi on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
